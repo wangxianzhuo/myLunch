f5bbc1d7543be8e377ca5f7e6e1ddb7f46254f34
shangjie <1722741150@qq.com>
2016-11-05 01:57:09 +0800

    configure from json & store data to hard disk

--- src/main/java/com/github/xianzhuo/myLunch/Lunch.java
+++ src/main/java/com/github/xianzhuo/myLunch/Lunch.java
@@ -1,6 +1,6 @@
 package com.github.xianzhuo.myLunch;
 
-import org.apache.log4j.PropertyConfigurator;
+import com.google.gson.reflect.TypeToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,44 +14,51 @@ import java.util.*;
  * 16-10-25
  */
 public class Lunch {
-    private static final String configPath = System.getProperty("user.dir") + "/config";
+    private static final String configPath = System.getProperty("user.dir") + "/data/config.json";
+    private static String dateStoredPath = "";
     private static final Logger LOG = LoggerFactory.getLogger(Lunch.class);
-    private static final int WEEK_DAY = 5;
+    private static int period;
+    private static final int DEFAULT_PERIOD = 5;
     private List<String> foods = new ArrayList<>();
     private List<String> menu = new ArrayList<>();
+    private Map configMap;
 
     public static void main(String... args) {
         Lunch lunch = new Lunch();
-        lunch.generateWeekMenu();
+//        lunch.generateWeekMenu();
         lunch.getTodayFood();
     }
 
     public Lunch() {
         initConfig();
+        initFoods();
+        initPeriod();
+        initDataStoredPath();
+
         LOG.info("Init Lunch - foods: [" + toStringList(foods) + "]");
         LOG.info("Init Lunch - menu: [" + toStringList(menu) + "]");
-        LOG.info("Init Lunch - week day: [" + WEEK_DAY + "]");
+        LOG.info("Init Lunch - period: [" + period + "]");
         LOG.info("Init Lunch finish.");
     }
 
     private void initConfig() {
-        File config = new File(configPath);
-        if (!config.exists()) {
-            LOG.error("can't find config file");
+        File configFile = new File(configPath);
+        if (!configFile.exists()) {
+            LOG.error("can't find config.json file");
             return;
         }
 
         FileReader fileReader = null;
         BufferedReader bufferedReader = null;
         try {
-            fileReader = new FileReader(config);
+            fileReader = new FileReader(configFile);
             bufferedReader = new BufferedReader(fileReader);
             String line;
+            String config = "";
             while ((line = bufferedReader.readLine()) != null) {
-                foods.add(line);
+                config += line;
             }
-        } catch (FileNotFoundException e) {
-            LOG.error(e.getMessage(), e);
+            configMap = GsonHelper.DEFAULT_GSON.fromJson(config, Map.class);
         } catch (IOException e) {
             LOG.error(e.getMessage(), e);
         } finally {
@@ -64,15 +71,92 @@ public class Lunch {
         }
     }
 
+    private void initFoods() {
+        if (configMap != null && configMap.containsKey("foods")) {
+            foods = (List<String>) configMap.get("foods");
+        }
+    }
+
+    private void initPeriod() {
+        if (configMap != null && configMap.containsKey("period")) {
+            period = new Double((Double) configMap.get("period")).intValue();
+        } else {
+            period = DEFAULT_PERIOD;
+        }
+    }
+
+    private void initDataStoredPath() {
+        if (configMap != null && configMap.containsKey("storedPath") &&
+                !((String) configMap.get("storedPath")).isEmpty()) {
+            dateStoredPath = (String) configMap.get("storedPath");
+        } else {
+            dateStoredPath = System.getProperty("user.dir") + "/data/data";
+        }
+    }
+
     public void generateWeekMenu() {
-        if (!menu.isEmpty()) {
+        if (menu.isEmpty() && new File(dateStoredPath).exists()) {
+            readMenuFromDataStoredFile();
+        } else {
             menu.clear();
+            for (int index = 0; index < period; index++) {
+                int random = (int) (1 + Math.random() * 10) % period;
+                menu.add(foods.get(random));
+            }
         }
-        for (int index = 0; index < WEEK_DAY; index++) {
-            int random = (int) (1 + Math.random() * 10) % WEEK_DAY;
-            menu.add(foods.get(random));
-        }
+        writeMenuToDataStoredFile();
         LOG.info("{menu: [" + toStringList(menu) + "]}");
+    }
+
+    private void writeMenuToDataStoredFile() {
+        File data = new File(dateStoredPath);
+        OutputStream fileOutputStream = null;
+        try {
+            if (!data.exists()) {
+                data.createNewFile();
+                fileOutputStream = new FileOutputStream(data);
+                fileOutputStream.write(GsonHelper.DEFAULT_GSON.toJson(menu).getBytes());
+            }
+        } catch (IOException e) {
+            LOG.error(e.getMessage(), e);
+        } finally {
+            try {
+                if (fileOutputStream != null) {
+                    fileOutputStream.close();
+                }
+            } catch (IOException e) {
+                LOG.error(e.getMessage(), e);
+            }
+        }
+    }
+
+    private void readMenuFromDataStoredFile() {
+        File data = new File(dateStoredPath);
+        BufferedReader bufferedReader = null;
+        try {
+            if (data.isFile() && data.exists()) {
+                bufferedReader = new BufferedReader(new FileReader(data));
+                String menuJson = "";
+                String tmp = "";
+                while ((tmp = bufferedReader.readLine()) != null) {
+                    menuJson += tmp;
+                }
+                if (menuJson != null && !menuJson.isEmpty()) {
+                    menu = GsonHelper.DEFAULT_GSON.fromJson(menuJson, new TypeToken<List<String>>() {
+                    }.getType());
+                }
+            }
+        } catch (IOException e) {
+            LOG.error(e.getMessage(), e);
+        } finally {
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (IOException e) {
+                LOG.error(e.getMessage(), e);
+            }
+        }
     }
 
     public String getTodayFood() {
@@ -80,16 +164,20 @@ public class Lunch {
         calendar.setTime(new Date());
 
         int weekDay = calendar.get(Calendar.DAY_OF_WEEK) - 1;
-        if (weekDay > 5) {
+        if (weekDay > period) {
             menu.clear();
-            LOG.info("Now [" + new SimpleDateFormat("yyyy-MM-dd HH:mm") + "] is weekend, i have to clear my menu.");
+            File data = new File(dateStoredPath);
+            if (data.exists()) {
+                calendar.add(Calendar.DATE, (weekDay - 1) * -1);
+                data.renameTo(new File(data.getAbsolutePath() + "." + new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime())));
+            }
+            LOG.info("Now [" + new SimpleDateFormat("yyyy-MM-dd HH:mm").format(new Date()) + "] is weekend, i have to clear my menu.");
             return "";
-        }
-        if (menu.isEmpty()) {
+        } else if (menu.isEmpty()) {
             generateWeekMenu();
         }
-        LOG.info("Today's food is [" + menu.get(weekDay) + "]");
-        return menu.get(weekDay);
+        LOG.info("Today's food is [" + menu.get(weekDay - 1) + "], and today is the [" + weekDay + "]th day of week.");
+        return menu.get(weekDay - 1);
     }
 
     public void promptFood() {
